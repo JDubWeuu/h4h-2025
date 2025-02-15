0f23a795b1f918135602769585934b468b01fe52
Jason Wu <wu80jason8@gmail.com>
2025-02-15 13:53:31 -0800

    ...

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,13 +1,24 @@
 import Image from "next/image";
 import "./globals.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [text, setText] = useState("record");
+
+  const clickButton = () => {
+    newText = text === "record" ? "stop recording" : "record"
+    setText(newText)
+  }
   return (
     <div>
       <h1>
         Blind Flights
       </h1>
-      
+      <main className="">
+        <button onClick={clickButton}>
+          {text}
+        </button>
+      </main>
       
       <button className="btn-88">
         <Image src="../public/testplane.svg" alt="Globe Icon" width={32} height={32} />
