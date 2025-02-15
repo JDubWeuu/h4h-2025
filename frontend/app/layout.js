68f051883a550e2b0def6e49f03236b24b29c4ae
Ariana Sun <arianasun23@gmail.com>
2025-02-15 13:46:26 -0800

    Started Next.js project

---
+++ frontend/app/layout.js
@@ -0,0 +1,27 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Visonairy",
+  description: "A website to help those with visual impairments to navigate their flight plans.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
